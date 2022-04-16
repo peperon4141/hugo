43862bbd8374d5c8a7fbd1314362a95264af68bc
peperon4141 <peperon4141@users.noreply.github.com>
2022-04-16 09:00:56 +0000

    deploy: 8a20add6889b1912810f98a459051693706e6d2b

--- search/data.js
+++ search/data.js
@@ -3,7 +3,7 @@ var data = [
 url: "https://tech.choihack.com/post/firebase/vue%E3%83%97%E3%83%AD%E3%82%B8%E3%82%A7%E3%82%AF%E3%83%88%E4%BD%9C%E6%88%90/",
 title: "Vueプロジェクト作成",
 date: "2022-04-16T17:36:40+09:00",
-body: "Vueプロジェクト作成 プロジェクト作成を楽したい プロジェクトを新規作成する場合に、ゼロから作成するよりも雛形を使いたい。 使用したいモジュールは以下。 Vue.js Webpack Firebase Sass TypeScript vue create コマンド：vue create 自分で選択肢を選ぶながらプロジェクトを作成できる。 最大の問題点は、webpackの設定ファイルがVue用に作成されたもので作られてしまう。 気に食わない。 vue init コマンド：vue init webpack-simple project-name 指定したテンプレートを元にプロジェクトを作成できる。 問題点は、パッケージのバージョンが古すぎる。 ローカルのNode.jsのバージョンとマッチしないと、作成直後でもエラーが発生して動作しない。 このコマンドはもう使えないと思って良いかもしれない。 webpack-cli コマンド：webpack init webpackのプロジェクトを作成することができる。 問題点は、Vue.jsの設定は自分でしないといけないこと。 結論 webpack-cli を使用するのが一番良さそう。 $ mkdir project-name $ cd project-name $ yarn init -y $ yarn add webpack webpack-cli $ webpack init 更に、これでプロジェクトを作成した後に、 firebase init (firebase-toolsが必要)でfirebaseプロジェクトの設定を追加するのが良さそう。"
+body: "Vueプロジェクト作成 プロジェクト作成を楽したい プロジェクトを新規作成する場合に、ゼロから作成するよりも雛形を使いたい。 使用したいモジュールは以下。 Vue.js Webpack Firebase Sass TypeScript vue create コマンド：vue create 自分で選択肢を選ぶながらプロジェクトを作成できる。 最大の問題点は、webpackの設定ファイルがVue用に作成されたもので作られてしまう。 気に食わない。 vue init コマンド：vue init webpack-simple project-name 指定したテンプレートを元にプロジェクトを作成できる。 問題点は、パッケージのバージョンが古すぎる。 ローカルのNode.jsのバージョンとマッチしないと、作成直後でもエラーが発生して動作しない。 このコマンドはもう使えないと思って良いかもしれない。 webpack-cli コマンド：webpack init webpackのプロジェクトを作成することができる。 問題点は、Vue.jsの設定は自分でしないといけないこと。 結論 webpack-cli を使用するのが一番良さそう。 $ mkdir project-name $ cd project-name $ yarn init -y $ yarn add webpack webpack-cli $ npx webpack init 更に、これでプロジェクトを作成した後に、 firebase init (firebase-toolsが必要)でfirebaseプロジェクトの設定を追加するのが良さそう。"
 },
 {
 url: "https://tech.choihack.com/post/gas/",
